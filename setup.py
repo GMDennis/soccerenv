91b4e1e3ab9a453748537b7be940fd0b1b57f4df
Gyungmin Dennis Kim <gyungmin.dennis.kim@gmail.com>
2019-11-10 07:50:57 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup, find_packages
+
+setup(name='soccerenv',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='emoudahi',
+      include_package_data=True,
+      url='https://github.com/GMDennis/soccerenv.git')
