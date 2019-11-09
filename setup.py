afec0276c8825143d24165fe9a21b62a0e104899
Gyungmin Dennis Kim <gyungmin.dennis.kim@gmail.com>
2019-11-10 08:06:04 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 
 from setuptools import setup, find_packages
+import sys, os.path
+
+sys.path.insert(0, os.path.join(osh.path.dirname(__file__), 'soccerenv'))
 
 setup(name='soccerenv',
       version='1.0',
-      description='Python Distribution Utilities',
+      description='Soccerenv described in correlated-Q Learning',
       author='emoudahi',
-      include_package_data=True,
+      author_email='',
+      license='',
       packages=setuptools.find_packages(),
+      zip_safe=False,
       url='https://github.com/GMDennis/soccerenv.git',
       python_requires='>=3.6')
